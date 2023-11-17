be1a4c6cfbb9bafb78e0e71230cd3d7efc0974f9
Analia Leyez <124401921+AnaliaLeyez@users.noreply.github.com>
2023-11-16 17:47:35 -0800

    delete copy file

--- src/controllers/mainControllers.js
+++
@@ -1,8 +0,0 @@
-const mainControllers= {
-    home: (req,res)=>res.send('Route for Home View'),
-    contact: (req, res)=> res.send('Rout for contact view'),
-    about: (req, res)=> res.send('Rout for about view'),
-    faqs: (req, res)=> res.send('Rout for faqs view')
-}
-
-module.exports = mainControllers;
