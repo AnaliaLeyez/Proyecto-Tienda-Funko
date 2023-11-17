0ebacfe3b27d7ccb98eb7093dec375af0eb609bb
Analia Leyez <124401921+AnaliaLeyez@users.noreply.github.com>
2023-11-16 16:31:08 -0800

    crontrollers cambia a controllers

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require ('express');
 const app = express();
 const mainRoutes = require('./src/routes/mainRoutes');
 const shopRoutes = require('./src/routes/shopRoutes');
+const adminRoutes = require('./src/routes/adminRoutes');
 
 app.use(express.static('public_html'));
 app.use('/', mainRoutes);
-app.use('/shop', shopRoutes);
+//app.use('/shop', shopRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(4000, ()=>console.log('servidor corriendo en http://localhost:4000'));
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,10 +1,10 @@
-const express = require ('express');
-const router = express.Router();
-const shopcontrollers= require('../controllers/shopCrontrollers')
+// const express = require ('express');
+// const router = express.Router();
+// const shopcontrollers= require('../controllers/shopControllers.js');
 
-router.get('/', shopcontrollers.shop);
-router.get('/item/:id', shopcontrollers.item);
-router.get('/cart', shopcontrollers.cart);
-router.get('/faqs', shopcontrollers.faqs);
+// router.get('/', shopcontrollers.shop);
+// router.get('/item/:id', shopcontrollers.item);
+// router.get('/cart', shopcontrollers.cart);
+// router.get('/faqs', shopcontrollers.faqs);
 
-module.exports =router;
+// module.exports =router;
