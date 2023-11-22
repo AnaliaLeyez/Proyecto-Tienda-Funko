cdf1d8f7e0fcfa097f24a5974370d399ccd76d65
Analia Leyez <124401921+AnaliaLeyez@users.noreply.github.com>
2023-11-22 11:33:21 -0800

    Agrego acceso a la ruta /create

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const mainRoutes = require('./src/routes/mainRoutes');
 const shopRoutes = require('./src/routes/shopRoutes');
 const adminRoutes = require('./src/routes/adminRoutes');
@@ -13,6 +14,10 @@ app.set('views', path.join(__dirname, './src/views'));
 //Middlewares POST (lo usamos siempre que tengamos que tomar datos de formulario)
 app.use(express.urlencoded({ extended : false })); //para poder obtener lo llenado en el formulario, obtener el req.body
 app.use(express.json()); //Indica si los datos vienen con formato JSON y lo pasa a objeto
+
+//Middleware para habilitar metodos PUT y DELETE a partir de hacer override
+app.use(methodOverride('_method'));
+
 
 //Middlewares Rutas
 app.use(express.static('public_html')); //debe fijarse por la carpeta public si exite el nombre indicado en la ruta. Ej, ver si existe /productos.html
--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,6 +1,7 @@
 const fs = require ('fs'); //file system permite leer archivos
 const path = require('path');
 
+
 const adminControllers= {
     admin: (req,res)=>{
         const productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
@@ -10,8 +11,17 @@ const adminControllers= {
             title: "ADMIN | FUNKOSHOP"
         },
     })},
-    createGET: (req, res)=> res.send('Rout for Admin Create'),
-    createPOST: (req, res)=> res.send('Rout POST for Admin Create'),
+    
+    createGET: (req, res)=> {
+        res.render("admin/create.ejs",{
+            view:{
+                title: "ADMIN | FUNKOSHOP"
+            },
+        })
+    },
+    
+    createPOST: (req, res)=> res.send('Rout for Admin create POST'),
+
     editGET: (req, res)=> res.send('Rout for Admin Edit'),
     editPUT: (req, res)=> res.send('Rout PUT for Admin Edit'),
     delete: (req, res)=> res.send('Rout for DELETE Admin')
